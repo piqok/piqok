b1f1bb427e8d857b50058914c1f6677e8e9d79f7
Gilad Kutiel <gilad.kutiel@gmail.com>
2020-01-26 21:51:26 +0200

    init

---
+++ dodo.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+
+def task_init():
+    return {
+        'actions': [f'mkdir -p dist build']
+    }
+
+
+def task_test():
+    return {
+        'actions': ['pytest'],
+        'file_dep': list(Path('src').glob('*.py')),
+    }
+
+
+def task_build():
+    return {
+        'actions': [
+            'python setup.py sdist bdist_wheel'
+        ],
+        'file_dep': ['VERSION'],
+    }
+
+
+def task_local_install():
+    return {
+        'actions': ['python setup.py install'],
+        'file_dep': ['dist'],
+    }
+
+
+def task_upload_test():
+    with open('VERSION') as f:
+        version = f.read()
+
+    whl = f'dist/srtrename-{version}-py3-none-any.whl'
+    return {
+        'actions': [
+            f'python -m twine upload --repository testpypi {whl}'
+        ],
+        'file_dep': [whl],
+        'verbosity': 2,
+    }
